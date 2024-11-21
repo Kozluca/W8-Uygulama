e4d6cf01080c99d918f996b67006d16c8826cdad
Ömer <okozluca.1@gmail.com>
2024-11-21 15:49:54 +0300

    Delete Week8_Pratik_Asp.Net_Core directory

--- Week8_Pratik_Asp.Net_Core/Week8_Pratik_Asp.Net_Core_Empty/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Week8_Pratik_Asp.Net_Core_Empty.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
